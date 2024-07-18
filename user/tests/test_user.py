47959b9936ee80d1a1c1b448e69aaac2f3cecc83
chaeeun1103 <chaeeun1103@inu.ac.kr>
2024-07-18 15:06:47 +0900

    test19

--- user/tests/test_user.py
+++ user/tests/test_user.py
@@ -1,9 +1,9 @@
-# user/tests/test_user.py
-
 import pytest
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
+from user.models import User
+
 
 @pytest.mark.integration
 @pytest.mark.django_db
@@ -12,6 +12,39 @@ class TestUserAPI:
         self.client = APIClient()
         self.create_nickname_url = reverse('user:create-nickname')
 
-    def test_create_nickname(self):  # 이름을 test_create_nickname으로 변경
+    def test_create_nickname(self):
         response = self.client.post(self.create_nickname_url, {'nickname': 'testuser'})
         assert response.status_code == status.HTTP_201_CREATED
+        assert response.data['data']['nickname'] == 'testuser'
+
+    def test_create_nickname_duplicate(self):
+        self.client.post(self.create_nickname_url, {'nickname': 'testuser'})
+        response = self.client.post(self.create_nickname_url, {'nickname': 'testuser'})
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert 'error' in response.data
+
+    def test_create_nickname_blank(self):
+        response = self.client.post(self.create_nickname_url, {'nickname': ''})
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert 'error' in response.data
+
+    def test_get_nickname(self):
+        create_response = self.client.post(self.create_nickname_url, {'nickname': 'testuser'})
+        user_id = create_response.data['data']['id']
+        get_nickname_url = reverse('user:get-nickname', kwargs={'user_id': user_id})
+        get_response = self.client.get(get_nickname_url)
+        assert get_response.status_code == status.HTTP_200_OK
+        assert get_response.data['data']['nickname'] == 'testuser'
+
+    def test_get_nonexistent_nickname(self):
+        get_nickname_url = reverse('user:get-nickname', kwargs={'user_id': 999})
+        response = self.client.get(get_nickname_url)
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+    def test_get_nickname_deleted_user(self):
+        create_response = self.client.post(self.create_nickname_url, {'nickname': 'testuser'})
+        user_id = create_response.data['data']['id']
+        User.objects.filter(id=user_id).update(is_deleted=True)
+        get_nickname_url = reverse('user:get-nickname', kwargs={'user_id': user_id})
+        response = self.client.get(get_nickname_url)
+        assert response.status_code == status.HTTP_404_NOT_FOUND
